45f99af39052b68d947d7546c4cfab25a6c15c5a
Ahmad Alqaisi <ahmedhamed1312@gmail.com>
2022-07-08 16:42:33 +0300

    adding main files

---
+++ src/context/app.context.jsx
@@ -0,0 +1,76 @@
+import { useReducer,createContext } from "react";
+
+export const AppContext=createContext({});
+
+const INITIAL_STATE={
+    input:"",
+    ip:"192.212.174.101",
+    position:[40.72091662099322,-74.00060176849367],
+    loaderVisability:false,
+    ipInfo:{
+        ip:'192.212.174.101',
+        city:'BROOKLYN',
+        country:'NY',
+        timezone:'UTC-500',
+        isp:'SpaceX Starlink'
+    }
+}
+const AppReducer=function(state,action){
+    const {type,payload}=action;
+    switch(type){
+        case "CHANGE_MAP_LOCATION":
+            return {...state,position:payload};
+        case "ON_INPUT_CHANGE":
+            return {...state,input:payload};
+        case "TOGGLE_LOADER":
+            return {...state,loaderVisability:payload}
+        case "CHANGE_IP_INFO":
+            return{...state,...payload}
+        default :
+            return {...state};
+    }
+}
+export function AppProvider({children}){
+    const [{input,ip,position,loaderVisability,ipInfo},dispatch]=useReducer(AppReducer,INITIAL_STATE);
+    
+    function changeMapPosition(evt){
+        dispatch({type:"CHANGE_MAP_LOCATION",payload:evt.latlng});
+    }
+
+    function onInputChange(evt){
+        dispatch({type:"ON_INPUT_CHANGE",payload:evt.target.value});
+    }
+
+    function changeIpInfo(){
+        dispatch({type:"TOGGLE_LOADER",payload:true});
+        fetch("https://hidden-citadel-03585.herokuapp.com/fetchIP",{
+            method:"post",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({
+                inputIp:input
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            const newState={
+                loaderVisability:false,
+                position:[data.location.lat,data.location.lng],
+                ipInfo:{
+                    ip:input,
+                    city:data.location.city,
+                    country:data.location.country,
+                    timezone:data.location.timezone,
+                    isp:data.isp
+                }
+            };
+            dispatch({type:"CHANGE_IP_INFO",payload:newState});
+        }).catch(err=>{
+            console.log(err);
+            alert('ADD A VALID IP ADDRESS OR CHECK YOUR INTERNET CONNECTION');
+            dispatch({type:"TOGGLE_LOADER",payload:false});
+        });
+    }
+    const value={ip,loaderVisability,ipInfo,position,changeIpInfo,changeMapPosition,onInputChange};
+    return(
+        <AppContext.Provider value={value}>{children}</AppContext.Provider>
+    );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { AppProvider } from './context/app.context'
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <AppProvider>
+        <App />
+    </AppProvider>
 );
 serviceWorkerRegistration.register();
